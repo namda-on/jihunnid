0591b0df1055950793b4712c837c858a8d3243e0
Jinyong Park <jinyoung1997@gmail.com>
2025-06-08 19:14:30 +0900

    animation 및 asset 수정

--- scripts/image.gd
+++ scripts/image.gd
@@ -5,22 +5,30 @@ extends Node
 @onready var outline_red = $"outline-red"
 @onready var infinite = $infinite
 @onready var outline_lg = $"outline-lg"
+@onready var anim_player = get_parent().get_node("AnimationPlayer")
+
+var mode = 'ready'
+
+func show_reveal() -> void:
+	print("show reveal")
+	mode = 'reveal'
+	anim_player.play("reveal")
 
 func show_result() -> void:
-	print("switch Scene")
+	mode = "result"
 	loading_text.visible = false
 	outline_red.visible = false
 	infinite.visible = true
 	outline_lg.modulate = Color(1,1,1,1)
-	var anim_player = get_parent().get_node("AnimationPlayer")
 	anim_player.play("showResult")	
 	
 func reset() -> void:
-	var anim_player = get_parent().get_node("AnimationPlayer")
-	anim_player.play("RESET")	
-	#start_text.visible = true
-	#start_text.modulate = Color(1,1,1,1)
-	#loading_text.visible = false
-	#outline_red.visible = false
-	#infinite.visible = false
-	#get_parent().disabled = false
+	anim_player.play("RESET")
+	start_text.visible = true
+	start_text.modulate = Color(1,1,1,1)
+	loading_text.visible = false
+	outline_red.visible = false
+	infinite.visible = false
+	get_parent().disabled = false
+	mode="ready"
+	
--- scripts/texture_button.gd
+++ scripts/texture_button.gd
@@ -1,18 +1,30 @@
 extends TextureButton
 
 @onready var anim_player = $AnimationPlayer
+@onready var anim_player2 = $AnimationPlayer2
 @onready var loading_text = $img/loading
 @onready var outline_red = $"img/outline-red"
 @onready var infinite = $img/infinite
 @onready var img = $img
 
+
 func _on_pressed() -> void:
+	if anim_player.is_playing():
+		return
+	
 	disabled = true
-	anim_player.play("clickAnim")
+	if img.mode == "ready":
+		print("reveal")
+		img.show_reveal()
+	elif img.mode == "reveal":
+		print("clickAnim")
+		anim_player.play("clickAnim")
+		#anim_player2.play("fade_in")		
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	disabled = false
 	if anim_name == "clickAnim":
 		anim_player.play("rotate")
-		await get_tree().create_timer(3).timeout
+		await get_tree().create_timer(10).timeout
 		anim_player.stop()
 		img.show_result()
